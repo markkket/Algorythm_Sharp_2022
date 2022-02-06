750db2b28b9be6aaaf859d6a7c805d6d64460389
Eugeny Konstantinov <jskonst@yandex.ru>
2022-02-06 21:42:04 +0300

    Added bubble sort sample with alfa tests (#2)
    * Added bubble sort sample with alfa tests
    
    * Fix for Test
    
    * Added all tests into sequential collection

--- CourseApp.Tests/Module2/BubbleSortTest.cs
+++ CourseApp.Tests/Module2/BubbleSortTest.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.IO;
 using Xunit;
-using CourseApp.Module1;
+using CourseApp.Module2;
 
-namespace CourseApp.Tests.Module1
+namespace CourseApp.Tests.Module2
 {
     [Collection("Sequential")]
-    public class AplusBTest : IDisposable
+    public class BubbleSortTest : IDisposable
     {
+        private const string Inp1 = @"7
+5 1 7 3 9 4 1";
+
+        private const string Inp2 = @"3
+-10 7 2";
+
         public void Dispose()
         {
             var standardOut = new StreamWriter(Console.OpenStandardOutput());
@@ -18,9 +24,8 @@ namespace CourseApp.Tests.Module1
         }
 
         [Theory]
-        [InlineData("10 12", "22")]
-        [InlineData("1 1", "2")]
-        [InlineData("10000 10000", "20000")]
+        [InlineData(Inp1, "1 1 3 4 5 7 9")]
+        [InlineData(Inp2, "-10 2 7")]
         public void Test1(string input, string expected)
         {
             var stringWriter = new StringWriter();
@@ -30,12 +35,11 @@ namespace CourseApp.Tests.Module1
             Console.SetIn(stringReader);
 
             // act
-            AplusB.Calculate();
+            BubbleSort.BubbleSortMethod();
 
             // assert
             var output = stringWriter.ToString().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
             Assert.Equal($"{expected}", output[0]);
-            var standardOutput = new StreamWriter(Console.OpenStandardOutput());
         }
     }
 }
